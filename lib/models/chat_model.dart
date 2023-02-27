8ecc11eaffce7213846cbb2e5dce2b0774eff781
Manish Pandit <manish.p@infosoftjoin.in>
2023-02-28 02:11:49 +0530

    Api implementation And Chat Screen implemented

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,1 @@
+class ChatModel{  final String msg;  final int chatIndex;  ChatModel({required this.msg,required this.chatIndex});  factory ChatModel.fromJson(Map<String, dynamic> json) =>      ChatModel(msg: json["message"], chatIndex: json["chatIndex"]);}
